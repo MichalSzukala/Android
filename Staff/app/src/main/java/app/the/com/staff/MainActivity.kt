29084b04fd8b41f68a8aa2e86d0932a74daf359f
Michal Szukala <szukalagithub@gmail.com>
2018-04-19 20:29:03 +0200

    Adding Image

--- Staff/app/src/main/java/app/the/com/staff/MainActivity.kt
+++ Staff/app/src/main/java/app/the/com/staff/MainActivity.kt
@@ -3,6 +3,7 @@ package app.the.com.staff
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,16 +16,21 @@ class MainActivity : AppCompatActivity() {
         buttonOK.setOnClickListener {
             textHaba.text = "Haba Haba " + editTextName.text.toString()
             sendToast("You pushed it")
-
         }
 
         //Clearing the text
         buttonClear.setOnClickListener {
             textHaba.text = ""
             editTextName.setText("")
+            imageViewPicasso.setImageResource(0)
         }
 
+        buttonPicture.setOnClickListener {
+            addingPictureFromInternet()
+        }
 
+        
+        addPicture()
     }
 
 
@@ -32,4 +38,18 @@ class MainActivity : AppCompatActivity() {
     fun sendToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
+
+
+    //Adding picture with Picasso
+    fun addingPictureFromInternet(){
+        val address: String = "https://upload.wikimedia.org/wikipedia/commons/7/79/Noon%2C_rest_from_work_-_Van_Gogh.jpeg"
+         Picasso.get().load(address).fit().centerCrop().into(imageViewPicasso)
+    }
+
+
+    //Adding picture with android setImageResource
+    fun addPicture(){
+        imageViewPicasso.setImageResource(R.drawable.duck)
+    }
+
 }
